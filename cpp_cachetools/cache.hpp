53c3b2409c5a30548ea611357646ebeb1dded732
Alessandro Lenzi <alessandro.lenzi@onfido.com>
2021-12-05 23:42:52 +0000

    Handle lambdas without std::function declaration

--- cpp_cachetools/cache.hpp
+++ cpp_cachetools/cache.hpp
@@ -3,7 +3,27 @@
 #include <functional>
 #include <mutex>
 #include "details.hpp"
+namespace details {
 
+    template<typename T>
+    struct get_std_funct_type : public get_std_funct_type
+            <decltype(&T::operator())>{};
+
+    template<typename C, typename Ret, typename... Args>
+    struct get_std_funct_type<Ret(C::*)(Args...) const> {
+        using func_type = typename std::function<Ret(Args...)>;
+    };
+
+    template<typename C, typename Ret, typename... Args>
+    struct get_std_funct_type<Ret(C::*)(Args...)> {
+        using func_type = typename std::function<Ret(Args...)>;
+    };
+
+    template<typename R, typename... Args>
+    struct get_std_funct_type<R(*)(Args...)> {
+        using func_type = typename std::function<R(Args...)>;
+    };
+}
 
 template<template<typename, typename> class CacheContainer>
 struct Cache {
@@ -44,6 +64,13 @@ struct Cache {
     template<typename... Args, typename... CacheBuildingArgs>
     static const std::function<void(Args...)> wrap(void (*function)(Args...), CacheBuildingArgs&&... cache_args) {
         std::function<void(Args...)> _function = function;
+        return wrap(_function, std::forward<CacheBuildingArgs&&>(cache_args)...);
+    }
+
+    template<typename T, typename... CacheBuildingArgs>
+    static const typename details::get_std_funct_type<typename std::decay<T>::type>::func_type wrap(T t, CacheBuildingArgs&&... cache_args) {
+        using _ftraits = details::get_std_funct_type<typename std::decay<T>::type>;
+        typename _ftraits::func_type _function = t;
         return wrap(_function, std::forward<CacheBuildingArgs&&>(cache_args)...);
     }
 
